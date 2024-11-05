0ea7f4f528b59469c92a4a69e9cee445f84186a7
TofPepper <etofunmi3@gmail.com>
2024-11-05 17:18:41 +0100

    Initial Commit

--- week2/arrow_functions/examples.dart
+++ week2/arrow_functions/examples.dart
@@ -6,6 +6,7 @@ void greetMe() => print("Hello, what do you have planned for the day?");
 void main(){
   greetMe();
 
+  /*
   //example 2, finding SI without arrow function
    // Principal amount, rate of interest, and time period
   double principal = 1000.0;
@@ -22,5 +23,21 @@ void main(){
 
   // Printing the result
   print("The simple interest is: \$${interest}");
+  */
+
+  //example 3, finding principal with arrow function
+  // Principal amount, rate of interest, and time period
+  double principal2 = 1000.0;
+  double rate2 = 5.0;
+  double time2 = 3.0;
+
+  // Arrow function to calculate simple interest
+  double calculateSimpleInterest2 (double p, double r, double t) => (p * r * t) / 100;
+
+  // Calling the function and storing the result
+  double interest2 = calculateSimpleInterest2(principal2, rate2, time2);
+
+  // Printing the result
+  print("The simple interest is: \$${interest2}");
 
 }
